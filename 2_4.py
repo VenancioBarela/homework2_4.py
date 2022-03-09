f937f231898acf9551b6b227a5d90f320d5f9dd0
Venancio Barela <calivee.ca@gmail.com>
2022-03-09 00:07:00 -0800

    3rd

---
+++ 2_4.py
@@ -0,0 +1,28 @@
+# list of numbers = [1, 10, 100]
+ 
+# print (list_of_number) = (1, 10, 1000)
+# list_of_numbers.clear()
+# print (list_of_numbers) #[]
+#  list_of_numbers.append (100)
+#  list_of_numbers.append (100)
+#  list_of_numbers.append (100)
+#  list_of_numbers.append (100)
+ 
+#  list of numbers2 = [3, 100, 5]
+ 
+#  list_of_numbers.extend (list_of_numbers2)
+#  print (list_of_numbers)
+ 
+#  l1 = [1, 32, 4]
+#  l2 = [100, 200, 300]
+#  l3 = l1 + l2
+#  print (l3)
+#  words =  ["spam", "wink", "Hi"]
+#  words.insert(len(words(, "Hello")
+ 
+#  print("words====>", words)
+distance=60
+time=60
+input = time 
+input = distance
+ditancetravelled=distance+time
